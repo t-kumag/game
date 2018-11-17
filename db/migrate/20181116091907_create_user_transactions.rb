b6a8c4184080509eb9e4f3704e7e9fa7f643e5ef
清水亮介 <shimizur@shimizur.local>
2018-11-17 22:16:58 +0900

    wip

--- db/migrate/20181116091907_create_user_transactions.rb
+++ db/migrate/20181116091907_create_user_transactions.rb
@@ -1,10 +1,17 @@
 class CreateUserTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :user_transactions do |t|
-      t.integer :log_user
+      t.integer :log_user_id
       t.integer :group_id
-      t.string :distribution_result
+      t.string :owner
 
+      t.integer :at_user_bank_transaction_id
+      t.integer :at_user_card_transaction_id
+      t.integer :at_user_emoney_transaction_id
+      t.integer :user_manually_created_transaction_id
+
+      # t.string :before_distributing_transaction_type # 一つ前のtransaction
+      # t.integer :before_distributing_transaction_id # 
       t.timestamps
     end
   end
