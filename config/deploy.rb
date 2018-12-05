62619f73a96135a9a3461357a572ecbfa145f108
清水亮介 <shimizur@shimizur.local>
2018-12-06 03:27:19 +0900

    wip

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,8 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       '/home/ec2-user/app/'
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind,       "unix:///tmp/sockets/puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
@@ -47,7 +48,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
+      # execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir /tmp/sockets -p"
       execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
