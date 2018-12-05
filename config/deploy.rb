a136e5219d6d217cfcd87fd529d5f5e0bd2dfed5
清水亮介 <shimizur@shimizur.local>
2018-12-06 00:47:54 +0900

    wip

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :rbenv_ruby, File.read('.ruby-version').strip
 # set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
-set :rbenv_map_bins, %w[rake gem bundle ruby rails]
+# set :rbenv_map_bins, %w[rake gem bundle ruby rails]
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'log',
   'tmp/pids',
