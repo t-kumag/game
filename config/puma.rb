82559538a54ca24c4157787e18c0dc80c66cd3b4
清水亮介 <shimizur@shimizur.local>
2018-12-06 03:22:02 +0900

    wip

--- config/puma.rb
+++ config/puma.rb
@@ -16,7 +16,8 @@ threads threads_count, threads_count
 # environment ENV.fetch("RAILS_ENV") { "development" }
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-bind "unix://home/ec2-user/app/shared/tmp/sockets/puma.sock"
+# bind "unix://home/ec2-user/app/shared/tmp/sockets/puma.sock"
+bind "unix://tmp/sockets/puma.sock"
 
 # _app_path = "#{File.expand_path("../..", __FILE__)}"
 # _app_name = File.basename(_app_path)
