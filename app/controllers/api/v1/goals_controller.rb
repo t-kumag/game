25b15282d84d7a56538b6e7bc93ca64246b15690
takushi ueda <takushi.ueda@gmail.com>
2019-04-03 18:37:41 +0900

    until post goal

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -10,11 +10,19 @@ class Api::V1::GoalsController < ApplicationController
   end
 
   def create
+    Entities::Goal.create(goal_params(params))
+    @goals = @current_user.groups.find(params[:group_id]).goals
+    render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
   def update
   end
 
   def destroy
+  end
+  
+  private
+  def goal_params(params)
+    params.permit(:name, :imageUrl, :startDate, :endDate, :goalAmount, :currentAmount).merge(user_id: @current_user.id, group_id: params[:group_id])
   end
 end
