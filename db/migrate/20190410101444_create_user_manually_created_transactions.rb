53a1419147b9127b25d8fcc159dd368208204873
ono <mac@mac-no-MacBook-Air.local>
2019-04-15 13:51:04 +0900

    明細テーブル追加

--- db/migrate/20190410101444_create_user_manually_created_transactions.rb
+++ db/migrate/20190410101444_create_user_manually_created_transactions.rb
@@ -4,7 +4,7 @@ class CreateUserManuallyCreatedTransactions < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true
       t.references :group, foreign_key: true
       t.references :at_transaction_category, index: { name: 'index_u_m_c_t_on_at_transaction_category_id' }, foreign_key: true
-      t.references :payment_method, polymorphic: true, index: { name: 'index_u_m_c_t_on_payment_method_type_and_payment_method_id' }
+      t.references :payment_method, index: { name: 'index_u_m_c_t_on_payment_method_id' }, foreign_key: true
       t.date :used_date, null: false
       t.string :title
       t.integer :amount
