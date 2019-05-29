5b2ae8d6e535549f60d3b73f9fdee433daf08a2b
nealle-fujiura <fujiura@nealle.com>
2019-05-29 14:13:09 +0900

    銀行と電子マネーについてAtTransactionをService化

--- app/controllers/api/v1/user/bank_transactions_controller.rb
+++ app/controllers/api/v1/user/bank_transactions_controller.rb
@@ -4,37 +4,17 @@ class Api::V1::User::BankTransactionsController < ApplicationController
   # TODO(fujiura): bank_account_id, transaction_id に対応するデータがないときの処理
 
   def index
-    bank = Entities::AtUserBankAccount.find(params[:bank_account_id])
-    @transactions = bank.at_user_bank_transactions.order(id: "DESC")
+    @transactions = Services::AtBankTransactionService.new.list(params[:bank_account_id])
     render 'list', formats: 'json', handlers: 'jbuilder'
   end
 
   def show
-    transaction = Entities::AtUserBankTransaction.find params[:id]
-    distributed = Entities::UserDistributedTransaction.find_by at_user_bank_transaction_id: transaction.id
-    bank = Entities::AtUserBankAccount.find params[:bank_account_id]
-    category = Entities::AtTransactionCategory.find distributed.at_transaction_category_id
-
-    @response = {
-      amount: distributed.amount,
-      at_transaction_category_id: distributed.at_transaction_category_id,
-      category_name1: category.category_name1,
-      category_name2: category.category_name2,
-      used_date: transaction.trade_date,
-      used_location: distributed.used_location,
-      is_shared: distributed.share,
-      payment_name: bank.fnc_nm + bank.brn_nm,
-    }
+    @response = Services::AtBankTransactionService.new.detail(params[:id], params[:bank_account_id])
     render 'show', formats: 'json', handlers: 'jbuilder'
   end
 
   def update
-    transaction = Entities::AtUserBankTransaction.find params[:id]
-    distributed = Entities::UserDistributedTransaction.find_by at_user_bank_transaction_id: transaction.id
-    distributed.at_transaction_category_id = params[:at_transaction_category_id]
-    distributed.used_location = params[:used_location]
-    distributed.share = params[:is_shared]
-    distributed.save!
+    @response = Services::AtBankTransactionService.new.update(params[:id], params[:at_transaction_category_id], params[:used_location], params[:is_shared])
 
     # TODO(fujiura): 何を返すべき？
     render 'update', formats: 'json', handlers: 'jbuilder'
--- app/views/api/v1/user/bank_transactions/list.json.jbuilder
+++ app/views/api/v1/user/bank_transactions/list.json.jbuilder
@@ -3,8 +3,8 @@
 json.app do
   json.array!(@transactions) do |transaction|
     json.transaction_id transaction.id
-    json.amount transaction.amount.to_f
-    json.date transaction.date
+    json.amount transaction.amount
+    json.date transaction.date.strftime('%Y-%m-%d %H:%M:%S')
     json.description transaction.description
   end
 end
