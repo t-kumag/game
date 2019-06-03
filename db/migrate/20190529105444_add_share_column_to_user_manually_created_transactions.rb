4b64296af9d34ff3a582832369fafd6546defb63
nealle-fujiura <fujiura@nealle.com>
2019-06-03 11:04:56 +0900

    shareカラムとgroup_idカラムを手動明細から削除

--- db/migrate/20190529105444_add_share_column_to_user_manually_created_transactions.rb
+++
@@ -1,5 +0,0 @@
-class AddShareColumnToUserManuallyCreatedTransactions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :user_manually_created_transactions, :share, :boolean
-  end
-end
---
+++ db/migrate/20190530093111_remove_column_from_user_manually_created_transactions.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromUserManuallyCreatedTransactions < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :user_manually_created_transactions, :group_id
+  end
+end
