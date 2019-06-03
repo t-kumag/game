ebfb711ae6206b960f3bafbc6e9987a13c43c99c
nealle-fujiura <fujiura@nealle.com>
2019-06-03 11:42:45 +0900

    removeマイグレーションがrollbackできるよう、type指定を追加

--- db/migrate/20190530093111_remove_column_from_user_manually_created_transactions.rb
+++ db/migrate/20190530093111_remove_column_from_user_manually_created_transactions.rb
@@ -1,5 +1,5 @@
 class RemoveColumnFromUserManuallyCreatedTransactions < ActiveRecord::Migration[5.2]
   def change
-    remove_column :user_manually_created_transactions, :group_id
+    remove_column :user_manually_created_transactions, :group_id, :bigint
   end
 end
