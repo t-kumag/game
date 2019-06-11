a959f3fa60f67802e1f9e002b1ebee52feea8f60
nealle-fujiura <fujiura@nealle.com>
2019-06-11 18:33:39 +0900

    :cop: rubocop

--- app/models/services/paring_service.rb
+++ app/models/services/paring_service.rb
@@ -3,12 +3,12 @@ class Services::ParingService
     @user = user
   end
 
-  def cancel()
+  def cancel
     # カレントユーザが所属するグループをすべて抽出
     groups = Entities::ParticipateGroup.where(user_id: @user.id).pluck(:group_id)
     # 同グループに所属するユーザをすべて抽出
     others = Entities::ParticipateGroup.where(group_id: groups).where.not(user_id: @user.id).pluck(:user_id)
-    
+
     # 自分含め同グループの UserDistributedTransaction の share フラグを false へ
     all_user_ids = others + [@user.id]
     bulk = []
@@ -21,17 +21,16 @@ class Services::ParingService
         }
       end
     end
-    Entities::UserDistributedTransaction.import [:id, :share, :used_date], 
-                                                bulk, 
+    Entities::UserDistributedTransaction.import [:id, :share, :used_date],
+                                                bulk,
                                                 on_duplicate_key_update: [:id, :share]
-   
+
     # グループに紐づく目標を削除
     Entities::Goal.where(group_id: groups).destroy_all
 
     # グループに所属するユーザーの共有口座削除
     all_user_ids.each do |user_id|
       user = Entities::User.find_by(id: user_id)
-      Rails.logger.debug user
       user&.at_user&.at_user_bank_accounts&.where(share: true)&.pluck(:id)&.each do |account_id|
         Services::AtUserService.new(user).delete_account(Entities::AtUserBankAccount, account_id)
       end
@@ -45,6 +44,5 @@ class Services::ParingService
 
     # グループに紐づく中間テーブルを削除
     Entities::ParticipateGroup.where(group_id: groups).destroy_all
-    
   end
 end
