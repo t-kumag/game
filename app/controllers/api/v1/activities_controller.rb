197e21caf0aa1340dcdd1bd79af7bd77ccbfeed7
Akinori ADACHI <adachi.c.akinori@gmail.com>
2019-06-12 15:16:59 +0900

    Misc: #104 不要な処理を削除

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::ActivitiesController < ApplicationController
   before_action :authenticate, except: :login
 
-  def create
-    Entities::Activity.add_bank_outcome_individual(Time.now, @current_user.id)
-#    render 'index', formats: 'json', handlers: 'jbuilder'
-  end
+#  def create
+#    Entities::Activity.add_bank_outcome_individual(Time.now, @current_user.id)
+#  end
 
   def index
     @activities = Entities::Activity.activities(@current_user.id)
