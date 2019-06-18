f616a178948b0c641d7cd6362e53354584005222
osidori-amemiya <kenji.amemiya@osidori.co>
2019-06-19 02:17:00 +0900

    [modify]記載場所の変更

--- config/routes.rb
+++ config/routes.rb
@@ -101,12 +101,12 @@ Rails.application.routes.draw do
 
       resources :users, only: [:create]
 
-      post 'user/confirm', to: 'users#confirm'
       get 'invite-url', :to => 'groups#invite_url'
       get 'user/at-url', :to => 'users#at_url'
       get 'user/at-sync', :to => 'users#at_sync'
       get 'user/at-token', :to => 'users#at_token'
       get 'user/activate', :to => 'users#activate'
+      post 'user/confirm', to: 'users#confirm'
 
       resources :budget_questions, path: '/budget-questions', only: [:create]
     end
