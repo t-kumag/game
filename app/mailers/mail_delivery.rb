91bbd68b4f7c3a27c4eedf8984bcaea76c2842aa
nealle-fujiura <fujiura@nealle.com>
2019-07-11 09:38:05 +0900

    連携メールエラー通知

--- app/mailers/mail_delivery.rb
+++ app/mailers/mail_delivery.rb
@@ -11,4 +11,24 @@ class MailDelivery < ApplicationMailer
     mail( :to => user.email,
           :subject => '【OsidOri】パスワードの再設定をしてください' )
   end
+  
+  def account_linkage_error(user, account)
+    @error_date = account.error_date.strftime("%Y年%m月%d日 %H時%M分")
+    @error_count = account.error_count
+    @fnc_nm = account.fnc_nm
+    @error_detail = account.last_rslt_msg
+
+    mail( :to => user.email,
+          :subject => '【OsidOri】口座連携エラーが発生しました' )
+  end
+
+  def skip_scraping(user, account)
+    @error_date = account.error_date.strftime("%Y年%m月%d日 %H時%M分")
+    @error_count = account.error_count
+    @fnc_nm = account.fnc_nm
+
+    mail( :to => user.email,
+          :subject => '【OsidOri】口座連携エラーのためスクレイピングスキップしました。' )
+  end
+
 end
