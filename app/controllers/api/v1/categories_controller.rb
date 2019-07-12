371cfd5ca940552889e736ef3c24e466df399eef
osidori-amemiya <kenji.amemiya@osidori.co>
2019-07-12 17:39:28 +0900

    [Add]小野さん指定のjsonに変更

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -6,10 +6,19 @@ class Api::V1::CategoriesController < ApplicationController
     @responses = []
 
     @categories_all.each do |ca|
+
+      transaction_categories = []
+      ca.at_transaction_category.each do |atc|
+        transaction_categories << {
+            at_transaction_category_id: atc.id,
+            at_transaction_category_category_name1: atc.category_name2
+        }
+      end
+
       @responses << {
           at_grouped_category_id: ca.id,
           at_grouped_category_name: ca.category_name,
-          at_transaction_categories: ca.at_transaction_category
+          at_transaction_categories: transaction_categories
       }
     end
 
