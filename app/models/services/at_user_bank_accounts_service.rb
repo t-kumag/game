b88986bb5af53b57977d2443f5985d8ba22a2401
osidori-amemiya <kenji.amemiya@osidori.co>
2019-07-17 03:19:07 +0900

    [Add]インデント修正

--- app/models/services/at_user_bank_accounts_service.rb
+++ app/models/services/at_user_bank_accounts_service.rb
@@ -1,11 +1,9 @@
 class Services::AtUserBankAccountsService
+  def self.get_balance(user)
+    Entities::AtUserBankAccount.find_by(at_user_id: user.id)
+  end
 
-    def self.get_balance(user)
-      Entities::AtUserBankAccount.find_by(at_user_id: user.id)
-    end
-
-    def self.get_balance_summary(group_id)
-      Entities::AtUserBankAccount.where(group_id: group_id, share: true)
-    end
-
+  def self.get_balance_summary(group_id)
+    Entities::AtUserBankAccount.where(group_id: group_id, share: true)
+  end
 end
