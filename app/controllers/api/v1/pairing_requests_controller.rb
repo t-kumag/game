273ea39635006d9f907418b27e49275e92c98770
osidori-amemiya <kenji.amemiya@osidori.co>
2019-07-21 22:21:20 +0900

    [Add]レビュー指摘箇所の修正

--- app/controllers/api/v1/pairing_requests_controller.rb
+++ app/controllers/api/v1/pairing_requests_controller.rb
@@ -46,19 +46,19 @@ class Api::V1::PairingRequestsController < ApplicationController
         puts "receive_pairing_request error =========="
         return
       end
-      if DateTime.now <= @pairing_request.token_expires_at
-        @pairing_request.to_user_id = @current_user.id
-        @pairing_request.status = 2
-        @pairing_request.save!
 
-        Entities::ParticipateGroup.create!({
-          group_id: @pairing_request.group_id,
-          user_id: @current_user.id
-        })
-        render json: {}, status: 200
-      else
-        render json: { errors: { code: '', message: "paring user not found or invalid token." } }, status: 422
-      end
+      return render json: { errors: { code: '', message: "paring user not found or invalid token." } }, status: 422  if DateTime.now > @pairing_request.token_expires_at
+
+      @pairing_request.to_user_id = @current_user.id
+      @pairing_request.status = 2
+      @pairing_request.save!
+
+      Entities::ParticipateGroup.create!({
+        group_id: @pairing_request.group_id,
+        user_id: @current_user.id
+      })
+
+      render json: {}, status: 200
     end
     # render 'receive_pairing_request', formats: 'json', handlers: 'jbuilder', status: 200
   end
