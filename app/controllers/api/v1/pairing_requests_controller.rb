4555cfd5a8c0c57f01a97e73f73d0f81d92709ef
dadgad126 <nijiguma@gmail.com>
2019-07-24 14:45:01 +0900

    ペアリングリクエストにgroup_idが入ってないバグを修正

--- app/controllers/api/v1/pairing_requests_controller.rb
+++ app/controllers/api/v1/pairing_requests_controller.rb
@@ -7,15 +7,15 @@ class Api::V1::PairingRequestsController < ApplicationController
     time = DateTime.now
     token = Digest::SHA256.hexdigest(@current_user.id.to_s + time.to_s + salt)
     @pairing_request = Entities::PairingRequest.create!({
-      from_user_id: @current_user.id,
-      token: token,
-      token_expires_at: DateTime.now + 7,
-      status: 1
-    })
+                                                            from_user_id: @current_user.id,
+                                                            token: token,
+                                                            token_expires_at: DateTime.now + 7,
+                                                            status: 1
+                                                        })
     render 'generate_pairing_token', formats: 'json', handlers: 'jbuilder', status: 200
   end
 
-  def receive_pairing_request    
+  def receive_pairing_request
 
     puts "receive_pairing_request =========="
     p params[:pairing_token]
@@ -23,8 +23,8 @@ class Api::V1::PairingRequestsController < ApplicationController
     begin
       ActiveRecord::Base.transaction do
         @pairing_request = Entities::PairingRequest.find_by(
-          token: params[:pairing_token],
-          status: 1
+            token: params[:pairing_token],
+            status: 1
         )
         unless @pairing_request
           return render json: { errors: { code: '', message: "pairing token not found." } }
@@ -34,16 +34,20 @@ class Api::V1::PairingRequestsController < ApplicationController
         return render json: { errors: { code: '', message: "paring user already exists" } }, status: 422  if @pairing_request.status.to_i == 2
         return render json: { errors: { code: '', message: "same user" } }, status: 422  if @pairing_request.from_user_id ==  @current_user.id
 
-        @pairing_request.to_user_id = @current_user.id
-        @pairing_request.status = 2
-        @pairing_request.save!
-
-        from_user_group = Entities::ParticipateGroup.find_by({user_id: [@pairing_request.from_user_id]})
+        from_user_group = Entities::ParticipateGroup.find_by({user_id: @pairing_request.from_user_id})
         return render json: { errors: { code: '', message: "from user group taken" } }, status: 422  if from_user_group.present?
-        to_user_group = Entities::ParticipateGroup.find_by({user_id: [@pairing_request.to_user_id]})
+        to_user_group = Entities::ParticipateGroup.find_by({user_id: @current_user.id})
         return render json: { errors: { code: '', message: "to user group taken" } }, status: 422  if to_user_group.present?
 
+        # group_id発行
         new_group = Entities::Group.create
+
+        @pairing_request.to_user_id = @current_user.id
+        @pairing_request.status = 2
+        @pairing_request.group_id = new_group.id
+        @pairing_request.save!
+
+        # group中間テーブル登録
         Entities::ParticipateGroup.create!({
           group_id: new_group.id,
           user_id: @pairing_request.from_user_id
@@ -55,10 +59,10 @@ class Api::V1::PairingRequestsController < ApplicationController
 
         render json: {}, status: 200
       end
-      rescue ActiveRecord::RecordInvalid => db_err
-        raise db_err
-      rescue => exception
-        raise exception
+    rescue ActiveRecord::RecordInvalid => db_err
+      raise db_err
+    rescue => exception
+      raise exception
     end
   end
 
