4e8da3bf6ce95e569db9241152f6b8103613e909
osidori-amemiya <kenji.amemiya@osidori.co>
2019-07-25 02:57:00 +0900

    [Add]口座削除でエラー500 #193

--- app/controllers/api/v1/user/bank_accounts_controller.rb
+++ app/controllers/api/v1/user/bank_accounts_controller.rb
@@ -63,7 +63,7 @@ class Api::V1::User::BankAccountsController < ApplicationController
   def destroy
     account_id = params[:id].to_i
     if @current_user.try(:at_user).try(:at_user_bank_accounts).pluck(:id).include?(account_id)
-      Services::AtUserService.new(@current_user).delete_account(Entities::AtUserBankAccount, account_id)
+      Services::AtUserService.new(@current_user).delete_account(Entities::AtUserBankAccount, [account_id])
     end
     render json: {}, status: 200
   end
