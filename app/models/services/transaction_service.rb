d514aa571b2525e2174cfde85153337d4c9a55a4
nealle-fujiura <fujiura@nealle.com>
2019-07-24 19:57:00 +0900

    家族ON/OFFロジック追加

--- app/models/services/transaction_service.rb
+++ app/models/services/transaction_service.rb
@@ -1,5 +1,5 @@
 class Services::TransactionService
-  def initialize(user_id, from, to, category_id)
+  def initialize(user_id, from, to, category_id, family, with_group=false)
     @user_id = user_id
 
     # from の 00:00:00 から to の 23:59:59 までのデータを取得
@@ -7,6 +7,8 @@ class Services::TransactionService
     @from = from ? Time.parse(from).beginning_of_day : Time.zone.today.beginning_of_month.beginning_of_day
     @to = to ? Time.parse(to).end_of_day : Time.zone.today.end_of_month.end_of_day
     @category_id = category_id
+    @family = family
+    @with_group = with_group
   end
 
   def type(transaction)
@@ -27,7 +29,7 @@ class Services::TransactionService
       bank_tarnsactions   = Entities::UserDistributedTransaction.joins(:at_user_bank_transaction).includes(:at_user_bank_transaction).where(user_id: @user_id, used_date: from..to)
       card_transactiohs   = Entities::UserDistributedTransaction.joins(:at_user_card_transaction).includes(:at_user_card_transaction).where(user_id: @user_id, used_date: from..to)
       emoney_transactiohs = Entities::UserDistributedTransaction.joins(:at_user_emoney_transaction).includes(:at_user_emoney_transaction).where(user_id: @user_id, used_date: from..to)
-      user_manually_created_transaction = Entities::UserDistributedTransaction.includes(:user_manually_created_transaction).where(user_id: @user_id, used_date: from..to)
+      user_manually_created_transaction = Entities::UserDistributedTransaction.joins(:user_manually_created_transaction).includes(:user_manually_created_transaction).where(user_id: @user_id, used_date: from..to)
     end
     bank_tarnsactions + card_transactiohs + emoney_transactiohs + user_manually_created_transaction
   end
@@ -55,9 +57,75 @@ class Services::TransactionService
   end
 
   def list(ids = @category_id)
-    transactions = fetch_transactions(@from, @to, ids)
-    transactions = generate_response_from_transactions transactions
-    sort_by_used_date transactions
+    if @with_group === true
+      transactions = fetch_transactions(@from, @to, ids)
+      # シェアしていない口座の明細 or シェアしていない明細を削除する
+      transactions = remove_not_shared_transaction transactions
+      transactions = generate_response_from_transactions transac
+      sort_by_used_date transactions
+    else
+      if @family === true
+        transactions = fetch_transactions(@from, @to, ids)
+        transactions = generate_response_from_transactions transactions
+        sort_by_used_date transactions
+      else
+        transactions = fetch_transactions(@from, @to, ids)
+        # シェアしている口座の明細 or シェアしている明細を削除する
+        transactions = remove_shared_transaction transactions
+        transactions = generate_response_from_transactions transactions
+        sort_by_used_date transactions
+      end
+    end
+  end
+
+  def remove_shared_transaction(transactions)
+    # シェアしている口座の明細 or シェアしている明細を削除する
+    transactions.reject do |t|
+      # シェアしている明細を削除する
+      if t.share === true
+        true
+      else
+        # シェアしていない明細、且つシェアしている口座
+        if t.try(:at_user_bank_transaction)
+          # 銀行
+          t.at_user_bank_transaction.at_user_bank_account.share === true 
+        elsif t.try(:at_user_card_transaction)
+          # カード
+          t.at_user_card_transaction.at_user_card_account.share === true
+        elsif t.try(:at_user_emoney_transaction)
+          # 電子マネー
+          t.at_user_emoney_transaction.at_user_emoney_service_account.share === true
+        else
+          # シェアしていない手動明細は除外しない
+          false
+        end
+      end
+    end
+  end
+
+  def remove_not_shared_transaction(transactions)
+    # シェアしていない口座の明細 or シェアしていない明細を削除する
+    transactions.reject do |t|
+      # シェアしている明細を削除しない
+      if t.share === true
+        false
+      else
+        # シェアしていない明細、且つシェアしていない口座
+        if t.try(:at_user_bank_transaction)
+          # 銀行
+          t.at_user_bank_transaction.at_user_bank_account.share === false
+        elsif t.try(:at_user_card_transaction)
+          # カード
+          t.at_user_card_transaction.at_user_card_account.share === false
+        elsif t.try(:at_user_emoney_transaction)
+          # 電子マネー
+          t.at_user_emoney_transaction.at_user_emoney_service_account.share === false
+        else
+          # シェアしていない手動明細は除外する
+          true
+        end
+      end
+    end
   end
 
   def grouped
