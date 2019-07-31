b9f56b0d0db32cee1557d33cb529c8b1fe6773e1
dadgad126 <nijiguma@gmail.com>
2019-07-31 17:22:29 +0900

    ・POST PUTでshareとis_sharedがあるのでshareに統一する ・明細のページングは基本from toを渡せるようにする。必要であればpageパラメータも渡せる。 ・明細の一覧と詳細のレスポンス内容を一覧の内容に揃える

--- app/controllers/api/v1/user/card_transactions_controller.rb
+++ app/controllers/api/v1/user/card_transactions_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::User::CardTransactionsController < ApplicationController
   before_action :authenticate
 
-  # TODO(fujiura): before_action で対象口座へのアクセス権があるかチェックする
-  # TODO(fujiura): card_account_id, transaction_id に対応するデータがないときの処理
-
   def index
-    @transactions = Services::AtCardTransactionService.new(@current_user).list(params[:card_account_id], params[:page])
+    @transactions = Services::AtCardTransactionService.new(@current_user, false, params[:from], params[:to]).list(params[:card_account_id], params[:page])
+    @categories   = Entities::AtTransactionCategory.all
     render json: {}, status: 200 and return if @transactions.blank?
     render 'list', formats: 'json', handlers: 'jbuilder'
   end
@@ -22,11 +20,11 @@ class Api::V1::User::CardTransactionsController < ApplicationController
         params[:id],
         params[:at_transaction_category_id],
         params[:used_location],
-        params[:is_shared],
-        params[:is_shared] ? @current_user.group_id : nil
+        params[:share],
+        params[:share] ? @current_user.group_id : nil
     )
     render json: {}, status: 200 and return if @response.blank?
-    # TODO(fujiura): 何を返すべき？
+
     render 'update', formats: 'json', handlers: 'jbuilder'
   end
 
