5898871376875f2ec85b9bc7c7512ee5457797ad
nealle-fujiura <fujiura@nealle.com>
2019-07-31 10:11:11 +0900

    メール認証の文言変更

--- app/mailers/mail_delivery.rb
+++ app/mailers/mail_delivery.rb
@@ -3,7 +3,7 @@ class MailDelivery < ApplicationMailer
   def user_registration(user)
     @token = user.token
     mail( :to => user.email,
-          :subject => '【OsidOri】メールアドレス認証を完了してはじめましょう' )
+          :subject => '【OsidOri】 登録を完了してはじめよう' )
   end
 
   def user_change_password_request(user)
