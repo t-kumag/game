31aa5902441f08d2fca2f07a8c4ebd5cfa7c4cf0
osidori-amemiya <kenji.amemiya@osidori.co>
2019-08-02 03:01:18 +0900

    [Add]ページネーション作成

--- app/controllers/api/v1/group/transactions_controller.rb
+++ app/controllers/api/v1/group/transactions_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::Group::TransactionsController < ApplicationController
     @response = []
    
     # 同じグループに種属するユーザの明細を自ユーザ含めてユーザごとに取得しマージする
-    @response += Services::TransactionService.new(@current_user, params[:from], params[:to], params[:category_id], false, true).list
-    @response += Services::TransactionService.new(@current_user.partner_user, params[:from], params[:to], params[:category_id], false, true).list
+    @response += Services::TransactionService.new(@current_user, params[:category_id], false, true, params[:page]).list
+    @response += Services::TransactionService.new(@current_user.partner_user, params[:category_id], false, true, params[:page]).list
 
     # TODO: マージした明細の時系列での並べ替え
     render 'list', formats: 'json', handlers: 'jbuilder'
@@ -16,8 +16,8 @@ class Api::V1::Group::TransactionsController < ApplicationController
     @response = []
 
     # 同じグループに種属するユーザの明細を自ユーザ含めてユーザごとに取得しマージする
-    @response += Services::TransactionService.new(@current_user, params[:from], params[:to], params[:category_id], false, true).grouped
-    @response += Services::TransactionService.new(@current_user.partner_user, params[:from], params[:to], params[:category_id], false, true).grouped
+    @response += Services::TransactionService.new(@current_user, params[:category_id], false, true, params[:page]).grouped
+    @response += Services::TransactionService.new(@current_user.partner_user, params[:category_id], false, true, params[:page]).grouped
 
     # TODO: マージした明細の時系列での並べ替え
     render 'list', formats: 'json', handlers: 'jbuilder'
