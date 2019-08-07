1b3812ce42e1e370bc0cc5a9f63ab92a154f48b0
dadgad126 <nijiguma@gmail.com>
2019-08-07 20:28:48 +0900

    /user/transactionsを収入、支出で分けて取れるように修正

--- app/controllers/api/v1/group/transactions_controller.rb
+++ app/controllers/api/v1/group/transactions_controller.rb
@@ -5,8 +5,25 @@ class Api::V1::Group::TransactionsController < ApplicationController
     @response = []
    
     # 同じグループに種属するユーザの明細を自ユーザ含めてユーザごとに取得しマージする
-    @response += Services::TransactionService.new(@current_user, params[:category_id], false, true, params[:from], params[:to]).list
-    @response += Services::TransactionService.new(@current_user.partner_user, params[:category_id], false, true, params[:from], params[:to]).list
+    @response += Services::TransactionService.new(
+        @current_user,
+        params[:category_id],
+        false,
+        params[:scope],
+        true,
+        params[:from],
+        params[:to]
+    ).list
+
+    @response += Services::TransactionService.new(
+        @current_user.partner_user,
+        params[:category_id],
+        false,
+        params[:scope],
+        true,
+        params[:from],
+        params[:to]
+    ).list
 
     # TODO: マージした明細の時系列での並べ替え
     render 'list', formats: 'json', handlers: 'jbuilder'
@@ -16,8 +33,26 @@ class Api::V1::Group::TransactionsController < ApplicationController
     @response = []
 
     # 同じグループに種属するユーザの明細を自ユーザ含めてユーザごとに取得しマージする
-    @response += Services::TransactionService.new(@current_user, params[:category_id], false, true, params[:from], params[:to]).grouped
-    @response += Services::TransactionService.new(@current_user.partner_user, params[:category_id], false, true, params[:from], params[:to]).grouped
+    @response += Services::TransactionService.new(
+        @current_user,
+        params[:category_id],
+        false,
+        params[:scope],
+        false,
+        true,
+        params[:from],
+        params[:to]
+    ).grouped
+
+    @response += Services::TransactionService.new(
+        @current_user.partner_user,
+        params[:category_id],
+        false,
+        params[:scope],
+        true,
+        params[:from],
+        params[:to]
+    ).grouped
 
     # TODO: マージした明細の時系列での並べ替え
     render 'list', formats: 'json', handlers: 'jbuilder'
--- app/controllers/api/v1/user/transactions_controller.rb
+++ app/controllers/api/v1/user/transactions_controller.rb
@@ -2,12 +2,28 @@ class Api::V1::User::TransactionsController < ApplicationController
   before_action :authenticate
 
   def index
-    @response = Services::TransactionService.new(@current_user, params[:category_id], params[:share], false, params[:from], params[:to]).list
+    @response = Services::TransactionService.new(
+        @current_user,
+        params[:category_id],
+        params[:share],
+        params[:scope],
+        false,
+        params[:from],
+        params[:to]
+    ).list
     render 'list', formats: 'json', handlers: 'jbuilder'
   end
 
   def grouped_transactions
-    @response = Services::TransactionService.new(@current_user, params[:category_id], params[:share], false, params[:from], params[:to]).grouped
+    @response = Services::TransactionService.new(
+        @current_user,
+        params[:category_id],
+        params[:share],
+        params[:scope],
+        false,
+        params[:from],
+        params[:to]
+    ).grouped
     render 'list', formats: 'json', handlers: 'jbuilder'
   end
 end
