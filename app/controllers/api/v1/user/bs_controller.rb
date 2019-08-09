670ed0841ff600b2104f20e370e6180812864b75
nealle-fujiura <fujiura@nealle.com>
2019-08-09 11:30:39 +0900

    個人のBSに電子マネーを追加

--- app/controllers/api/v1/user/bs_controller.rb
+++ app/controllers/api/v1/user/bs_controller.rb
@@ -2,16 +2,24 @@ class Api::V1::User::BsController < ApplicationController
   before_action :authenticate
 
   def summary
-    amount = 0
+    bank_amount = 0
+    emoney_amount = 0
     goal_amount = 0
 
     at_bank_accounts = @current_user.try(:at_user).try(:at_user_bank_accounts).where(at_user_bank_accounts: {share: false})
+    at_emoney_accounts = @current_user.try(:at_user).try(:at_user_emoney_service_accounts).where(at_user_emoney_service_accounts: {share: false})
+    
     if at_bank_accounts
-      amount = at_bank_accounts.sum{|i| i.balance}
+      bank_amount = at_bank_accounts.sum{|i| i.balance}
       goal_amount = Services::GoalService.new(@current_user).goal_amount(at_bank_accounts.pluck(:id))
     end
+    
+    if at_emoney_accounts
+      emoney_amount = at_emoney_accounts.sum{|i| i.balance}
+    end
+
     @response = {
-        amount: amount - goal_amount
+        amount: bank_amount + emoney_amount - goal_amount
     }
     render 'summary', formats: 'json', handlers: 'jbuilder'
   end
