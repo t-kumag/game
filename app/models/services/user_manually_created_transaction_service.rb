695a5e37bed705606681fb9c4d39b7803ca14d70
osidori-amemiya <kenji.amemiya@osidori.co>
2019-08-20 01:37:22 +0900

    [Add]shareだけでも更新されるように修正

--- app/models/services/user_manually_created_transaction_service.rb
+++ app/models/services/user_manually_created_transaction_service.rb
@@ -21,18 +21,21 @@ class Services::UserManuallyCreatedTransactionService
   end
 
   def update_user_manually_created(options)
-    Entities::UserDistributedTransaction.update!(
-        user_id: @transaction.user_id,
-        used_date: @transaction.used_date,
-        group_id: options.has_key?(:group_id) ? options[:group_id] : nil,
-        share: options.has_key?(:share) ? options[:share] : false,
-        at_user_bank_transaction_id: nil,
-        at_user_card_transaction_id: nil,
-        at_user_emoney_transaction_id: nil,
-        user_manually_created_transaction_id: @transaction.id,
-        used_location: @transaction.used_location,
-        amount: @transaction.amount,
-        at_transaction_category_id:  @transaction.at_transaction_category_id
+    user_distribute_transaction = Entities::UserDistributedTransaction.find_by(user_manually_created_transaction_id:  @transaction.id, user_id: @transaction.user_id)
+    user_distribute_transaction.update!(
+        {
+            user_id: @transaction.user_id,
+            used_date: @transaction.used_date,
+            group_id: options.has_key?(:group_id) ? options[:group_id] : nil,
+            share: options.has_key?(:share) ? options[:share] : false,
+            at_user_bank_transaction_id: nil,
+            at_user_card_transaction_id: nil,
+            at_user_emoney_transaction_id: nil,
+            user_manually_created_transaction_id: @transaction.id,
+            used_location: @transaction.used_location,
+            amount: @transaction.amount,
+            at_transaction_category_id:  @transaction.at_transaction_category_id
+        }
     )
   end
 
