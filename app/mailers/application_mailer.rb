0620d6b0a881427b1ebfbfe77920c468ca7eac01
osidori-ono <f.ppuci@gmail.com>
2019-08-29 18:46:08 +0900

    mail文字化け対応 エンコード追加

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Settings.mail_setting_from, content_type: Settings.mail_content_type, parts_order: ["text/html", "text/plain"]
+  default from: Settings.mail_setting_from,
+                content_type: Settings.mail_content_type,
+                parts_order: ["text/html", "text/plain"],
+                charset: Settings.mail_charset
 end
