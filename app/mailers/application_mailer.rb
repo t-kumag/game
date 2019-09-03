825dd79da5eb13c3466032e114d075eff8ddeac2
osidori-ono <f.ppuci@gmail.com>
2019-09-03 18:32:30 +0900

    テキストメールの対応

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: Settings.mail_setting_from,
                 content_type: Settings.mail_content_type,
-                parts_order: ["text/html", "text/plain"],
+                parts_order: ["text/plain","text/html"],
                 charset: Settings.mail_charset
 end
