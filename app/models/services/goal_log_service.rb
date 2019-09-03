c1655f0fb57ef7f04329b350c92067fb2e7b5f14
osidori-amemiya <kenji.amemiya@osidori.co>
2019-09-03 13:41:35 +0300

    静的メソッドに変換

--- app/models/services/goal_log_service.rb
+++ app/models/services/goal_log_service.rb
@@ -76,7 +76,7 @@ class Services::GoalLogService
     }
   end
 
-  def alreday_exist_first_amount(goal_id, user_id)
+  def self.alreday_exist_first_amount(goal_id, user_id)
     Entities::GoalLog.where(goal_id: goal_id, user_id: user_id).pluck(:first_amount).present?
   end
 end
