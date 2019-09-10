75381cb3b4fff42798f1505a13b9876f727030a4
osidori-amemiya <kenji.amemiya@osidori.co>
2019-09-10 06:52:46 +0300

    current_userが認識されてない問題の解決

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -160,8 +160,8 @@ class Api::V1::UsersController < ApplicationController
           # 退会理由を記載する
           register_cancel_reasons(cancel_checklists, cancel_reason)
           # 削除対象のテーブル
-          # at_user_tokens at_users users
-          if @user.try(:at_user).try(:token).present?
+          # DB_TABLE: at_user_tokens at_users users
+          if @current_user.try(:at_user).try(:token).present?
             @current_user.at_user.at_user_tokens.destroy_all
             @current_user.at_user.destroy
           end
