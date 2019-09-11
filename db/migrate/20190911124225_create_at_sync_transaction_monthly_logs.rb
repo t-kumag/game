b0fbafd9fc3aa48797eed9090577c2e0580a0a8f
osidori-amemiya <kenji.amemiya@osidori.co>
2019-09-11 16:22:50 +0300

    datetimeの方がより細く情報が取れるので

--- db/migrate/20190911124225_create_at_sync_transaction_monthly_logs.rb
+++ db/migrate/20190911124225_create_at_sync_transaction_monthly_logs.rb
@@ -1,7 +1,7 @@
 class CreateAtSyncTransactionMonthlyLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :at_sync_transaction_monthly_logs do |t|
-      t.date :monthly_date
+      t.datetime :monthly_date, null: false
       t.references :at_user_bank_account, foreign_key: true
       t.references :at_user_card_account, foreign_key: true
       t.references :at_user_emoney_service_account, index: { name: 'index_a_s_t_l_on_at_user_emoney_service_account_id' }, foreign_key: true
