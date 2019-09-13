8b1fbc541f84f3422a08ae3655433f27408da10e
osidori-amemiya <kenji.amemiya@osidori.co>
2019-09-13 14:48:24 +0300

    refactor functtion name

--- app/models/services/at_sync_transaction_monthly_date_log_service.rb
+++ app/models/services/at_sync_transaction_monthly_date_log_service.rb
@@ -1,14 +1,13 @@
 class Services::AtSyncTransactionMonthlyDateLogService
 
-  def self.all_transaction_monthly_date(account_id, to, at_user_type)
-    next_day =  to.next_day(1)
+  def self.fetch_monthly_transaction_date_from_specified_date(account_id, from, at_user_type)
     case at_user_type
     when "at_user_card_account"
-      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_card_account_id: account_id).where("monthly_date <= :last_date", last_date: next_day).pluck("monthly_date")
+      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_card_account_id: account_id).where("monthly_date <= :last_date", last_date: from).pluck("monthly_date")
     when "at_user_bank_account"
-      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_bank_account_id: account_id).where("monthly_date <= :last_date", last_date: next_day).pluck("monthly_date")
+      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_bank_account_id: account_id).where("monthly_date <= :last_date", last_date: from).pluck("monthly_date")
     when "at_user_emoney_service_account"
-      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_emoney_service_account_id: account_id).where("monthly_date <= :last_date", last_date: next_day).pluck("monthly_date")
+      Entities::AtSyncTransactionMonthlyDateLog.order(monthly_date: :desc).where(at_user_emoney_service_account_id: account_id).where("monthly_date <= :last_date", last_date: from).pluck("monthly_date")
     end
   end
 
