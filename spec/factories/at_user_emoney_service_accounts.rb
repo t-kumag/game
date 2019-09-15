c534737e56c4c357ba4149e29811a4cf2e345699
nealle-fujiura <fujiura@nealle.com>
2019-09-15 19:40:13 +0900

    modify indent

--- spec/factories/at_user_emoney_service_accounts.rb
+++ spec/factories/at_user_emoney_service_accounts.rb
@@ -11,10 +11,10 @@ FactoryBot.define do
     acct_no              { "test" }
     memo                 { "test" }
     use_yn               { "Y" }
-    cert_type	           { "1" }
-    scrap_dtm	           { "2019/08/22 00:00:00" }
+    cert_type            { "1" }
+    scrap_dtm            { "2019/08/22 00:00:00" }
     last_rslt_cd         { 0 }
-    last_rslt_msg	       { "正常" }
+    last_rslt_msg        { "正常" }
     deleted_at           { nil }
     group_id             { nil }
     error_date           { nil }
