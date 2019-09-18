af13c5e23aa9a3e085d72dc1d7efe2a6c1cf40e6
nealle-fujiura <fujiura@nealle.com>
2019-09-18 16:37:11 +0900

    Change position of describe

--- spec/requests/api/v1/notices_spec.rb
+++ spec/requests/api/v1/notices_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'notices_controller' do
+  let(:user) { create(:user) } 
+  let(:headers) { { Authorization: 'Bearer ' + user.token } } 
+  let(:params) { { title: 'test', date: '2020/01/01', url: 'https://www.osidori.co/support' } } 
+  
   describe '#create' do
-    let(:user) { create(:user) } 
-    let(:headers) { { Authorization: 'Bearer ' + user.token } } 
-    let(:params) { { title: 'test', date: '2020/01/01', url: 'https://www.osidori.co/support' } } 
-    
     context 'success' do
       it 'response 200' do
         post '/api/v1/notices', params: params, headers: headers 
