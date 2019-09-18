54cb8306a51d2db1fe81050d071c201f0c5038fb
nealle-fujiura <fujiura@nealle.com>
2019-09-18 21:44:55 +0900

    modify @ to let

--- spec/requests/api/v1/user/bank_accounts_spec.rb
+++ spec/requests/api/v1/user/bank_accounts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'bank_accounts_controller' do
   let(:user) { create(:user, :with_at_user) }
-  let(:headers) { { "Authorization" => "Bearer " + user.token} }
+  let(:headers) { { 'Authorization' => 'Bearer ' + user.token} }
   let(:params) { { share: false} }
   let(:at_user_bank_account) { create(:at_user_bank_account, at_user_id: user.at_user.id, share: true) }
   
@@ -30,12 +30,11 @@ RSpec.describe 'bank_accounts_controller' do
       end
       
       # 口座削除のテストは実行する場合、
-      # delete "/api/v1/user/bank-accounts/#{at_user_bank_account.id}", headers: headers
       # osidori_api/app/models/services/at_user_service.rbの
-      # def initialize の10行目と
-      # def delete_account の211～216行目をコメントアウト
+      # def initialize の10行目と def delete_account の211～216行目をコメントアウト
       # it 'at_user_bank_account is nil' do
-      #  expect(Entities::AtUserBankAccount.find_by(id: id)).to eq nil
+      #  delete "/api/v1/user/bank-accounts/#{at_user_bank_account.id}", headers: headers
+      #  expect(Entities::AtUserBankAccount.find_by(id: at_user_bank_account.id)).to eq nil
       # end
     end
   end
--- spec/requests/api/v1/user/card_accounts_spec.rb
+++ spec/requests/api/v1/user/card_accounts_spec.rb
@@ -1,38 +1,41 @@
 require 'rails_helper'
 
-describe 'card_accounts_controller' do
-  before(:each) do
-    @user = create(:user, :with_at_user)
-    @headers = { "Authorization" => "Bearer " + @user.token}
+RSpec.describe 'card_accounts_controller' do
+  let(:user) { create(:user, :with_at_user) }
+  let(:headers) { { 'Authorization' => 'Bearer ' + user.token} }
+  let(:params) { { share: false} }
+  let(:at_user_card_account) { create(:at_user_card_account, at_user_id: user.at_user.id, share: true) }
+
+  describe '#update' do
+    let(:at_user_card_account_after_update) { Entities::AtUserCardAccount.find(at_user_card_account.id) }
+
+    context 'success' do
+      it 'response 200' do
+        put "/api/v1/user/card-accounts/#{at_user_card_account.id}", params: params, headers: headers
+        expect(response.status).to eq 200
+      end
+
+      it 'share is updated' do
+        put "/api/v1/user/card-accounts/#{at_user_card_account.id}", params: params, headers: headers
+        expect(at_user_card_account_after_update.share).to eq params[:share]
+      end
+    end
   end
 
-  it 'PUT #update' do
-    at_user_card_account = create(:at_user_card_account, at_user_id: @user.at_user.id, share: true)
-    
-    params = {
-      "share" => false
-    }
-    id = at_user_card_account.id
-    
-    put "/api/v1/user/card-accounts/#{id}", params: params, headers: @headers
-
-    @at_user_card_account = Entities::AtUserCardAccount.find(id)
-
-    expect(@at_user_card_account.share).to eq params["share"]
-    expect(response.status).to eq 200
-  end
-
-  it 'DELETE #destroy' do
-    at_user_card_account = create(:at_user_card_account, at_user_id: @user.at_user.id)
-    id = at_user_card_account.id
-    
-    delete "/api/v1/user/card-accounts/#{id}", headers: @headers
-    
-    # ↓コメントアウトされたテストは実行する場合、
-    # osidori_api/app/models/services/at_user_service.rbの
-    # def initialize の10行目と
-    # def delete_account の211～216行目をコメントアウトする
-    # expect(Entities::AtUserCardAccount.find_by(id: id)).to eq nil
-    expect(response.status).to eq 200
+  describe '#destroy' do
+    context 'success' do
+      it 'response 200' do
+        delete "/api/v1/user/card-accounts/#{at_user_card_account.id}", headers: headers
+        expect(response.status).to eq 200
+      end
+      
+      # 口座削除のテストは実行する場合、
+      # osidori_api/app/models/services/at_user_service.rbの
+      # def initialize の10行目と def delete_account の211～216行目をコメントアウト
+      # it 'at_user_card_account is nil' do
+      #  delete "/api/v1/user/card-accounts/#{at_user_card_account.id}", headers: headers
+      #  expect(Entities::AtUserCardAccount.find_by(id: at_user_card_account.id)).to eq nil
+      # end
+    end
   end
 end
