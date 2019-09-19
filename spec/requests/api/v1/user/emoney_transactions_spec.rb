71e93354daabe737adb20ad23a5f2fb776fdaeb8
nealle-fujiura <fujiura@nealle.com>
2019-09-19 12:42:38 +0900

    modify @ to let

--- spec/requests/api/v1/user/emoney_transactions_spec.rb
+++ spec/requests/api/v1/user/emoney_transactions_spec.rb
@@ -1,40 +1,34 @@
 require 'rails_helper'
 
-describe 'emoney_transactions_controller' do
-  before(:each) do
-    @user = create(:user, :with_at_user_emoney_accounts)
-    @headers = { "Authorization" => "Bearer " + @user.token}
+RSpec.describe 'emoney_transactions_controller' do
+  let(:user) { create(:user, :with_at_user_emoney_transactions) }
+  let(:headers) { { 'Authorization': 'Bearer ' + user.token} }
+  let(:params) { { 
+    at_transaction_category_id: 1,
+    used_location: 'ミロク居酒屋 支払',
+    share: false
+  } }
+  let(:at_user_emoney_service_account) { user.at_user.at_user_emoney_service_accounts.first }
+  let(:at_user_emoney_transaction) { at_user_emoney_service_account.at_user_emoney_transactions.first }
+  let(:user_distributed_transaction_after_update) { 
+    Entities::UserDistributedTransaction.find_by(at_user_emoney_transaction_id: at_user_emoney_transaction.id)
+  }
+  let!(:at_grouped_category) { create(:at_grouped_category) } 
+  let!(:at_transaction_category) { create(:at_transaction_category) }
+
+  describe '#update' do
+    context 'success' do
+      it 'response 200' do
+        put "/api/v1/user/emoney-accounts/#{at_user_emoney_service_account.id}/transactions/#{at_user_emoney_transaction.id}", 
+          params: params, headers: headers
+        expect(response.status).to eq 200
+      end
+
+      it 'used_location is updated' do
+        put "/api/v1/user/emoney-accounts/#{at_user_emoney_service_account.id}/transactions/#{at_user_emoney_transaction.id}", 
+          params: params, headers: headers
+        expect(user_distributed_transaction_after_update.used_location).to eq params[:used_location]
+      end
+    end
   end
-
-  it 'PUT #update' do
-    create(:at_grouped_category)
-    create(:at_transaction_category)
-    @at_user_emoney_service_account = @user.at_user.at_user_emoney_service_accounts.first
-    @at_user_emoney_transaction = create(
-      :at_user_emoney_transaction, 
-      at_user_emoney_service_account_id: @at_user_emoney_service_account.id
-    )
-    @at_user_emoney_transaction.user_distributed_transaction = create(
-      :user_distributed_transaction, 
-      user_id: @user.id, 
-      at_user_emoney_transaction_id: @at_user_emoney_transaction.id
-    )
-
-    params = {
-      "at_transaction_category_id" => 1,
-      "used_location" => "OsidOri",
-      "share" => false
-    }
-    emoney_account_id = @at_user_emoney_service_account.id
-    transaction_id = @at_user_emoney_transaction.id
-
-    put "/api/v1/user/emoney-accounts/#{emoney_account_id}/transactions/#{transaction_id}", params: params, headers: @headers
-
-    @user_distributed_transaction = Entities::UserDistributedTransaction.find_by(user_id: @user.id)
-
-    expect(@user_distributed_transaction.used_location).to eq params["used_location"]
-
-    expect(response.status).to eq 200
-  end
-
 end
