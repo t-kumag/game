9eb677ecea41284c9ae11703f8ecc94171895414
nealle-fujiura <fujiura@nealle.com>
2019-09-20 13:55:36 +0900

    add users#change_password

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -4,11 +4,12 @@ RSpec.describe 'users_controller' do
   let(:user) { create(:user) }
   let(:headers) { { Authorization: 'Bearer ' + user.token } }
   let(:params) { { email: user.email } } 
-  let(:user_after_create) { Entities::User.find_by(email: params[:email]) }
-
+  let(:user_after_create) { Entities::User.find(user.id) }
+  
   describe '#create' do
     let(:params) { { email: 'test1@example.com', password: 'testtest' } } 
-
+    let(:user_after_create) { Entities::User.find_by(email: params[:email]) }
+  
     context 'success' do
       it 'response 200' do
         post '/api/v1/users', params: params 
@@ -48,6 +49,28 @@ RSpec.describe 'users_controller' do
       it 'user_after_create.token is not equal user.token' do
         post '/api/v1/users/change_password_request', params: params, headers: headers
         expect(user_after_create.token).not_to eq user.token
+      end
+    end
+  end
+
+  describe '#change_password' do
+  
+    let(:user) { create(:user, password: 'testtest') } 
+    let(:params) { { 
+      token: user.token,
+      password: user.password,
+      password_confirm: user.password,
+    } } 
+
+    context 'success' do
+      it 'response 200' do
+        post '/api/v1/users/change_password', params: params, headers: headers 
+        expect(response.status).to eq 200
+      end
+
+      it 'password_digest is updated' do
+        post '/api/v1/users/change_password', params: params, headers: headers
+        expect(user_after_create.password_digest).not_to eq user.password_digest
       end
     end
   end
