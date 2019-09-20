e169712e4f30d38d339f627e1d9e0b36d1b87b91
nealle-fujiura <fujiura@nealle.com>
2019-09-20 13:29:25 +0900

    add users#change_password_request

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'users_controller' do
+  let(:user) { create(:user) }
+  let(:headers) { { Authorization: 'Bearer ' + user.token } }
+  let(:params) { { email: user.email } } 
+  let(:user_after_create) { Entities::User.find_by(email: params[:email]) }
+
   describe '#create' do
     let(:params) { { email: 'test1@example.com', password: 'testtest' } } 
-    let(:user_after_create) { Entities::User.find_by(email: params[:email]) }
 
     context 'success' do
       it 'response 200' do
@@ -25,13 +29,25 @@ RSpec.describe 'users_controller' do
 
   describe '#resend' do
     let(:user) { create(:user, email_authenticated: 0) }
-    let(:headers) { { Authorization: 'Bearer ' + user.token } }
-    let(:params) { { email: 'test1@example.com'} } 
 
     context 'success' do
       it 'response 200' do
         post '/api/v1/user/resend', params: params, headers: headers 
         expect(response.status).to eq 200
+      end
+    end
+  end
+
+  describe '#change_password_request' do
+    context 'success' do
+      it 'response 200' do
+        post '/api/v1/users/change_password_request', params: params, headers: headers 
+        expect(response.status).to eq 200
+      end
+
+      it 'user_after_create.token is not equal user.token' do
+        post '/api/v1/users/change_password_request', params: params, headers: headers
+        expect(user_after_create.token).not_to eq user.token
       end
     end
   end
