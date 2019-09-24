c82ab1a3e17b8bf89f75e65b342259f60d3a8b64
nealle-fujiura <fujiura@nealle.com>
2019-09-24 11:43:54 +0900

    modify indent

--- spec/factories/pairing_requests.rb
+++ spec/factories/pairing_requests.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :pairing_request, :class => Entities::PairingRequest do
-    from_user_id	   { nil }
-    to_user_id		   { nil }
-    group_id    	   { nil }
+    from_user_id     { nil }
+    to_user_id       { nil }
+    group_id         { nil }
     token            { "test" }
     token_expires_at { "2019/12/31 23:59:59" }
     status           { 0 }
