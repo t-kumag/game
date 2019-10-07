3e5edfb9eff1eb10dd80ce3503d95e7b54784b71
osidori-amemiya <kenji.amemiya@osidori.co>
2019-10-07 13:07:43 +0300

    edit config

--- config/initializers/activity.rb
+++ config/initializers/activity.rb
@@ -4,7 +4,7 @@ module ACTIVITY_TYPE
       'pairing_created'               => { message: 'ペアリングが完了しました。', url: nil},
       'financial_account_faild'       => { message: '連携に失敗している金融機関があります', url: 'osidori://financial-accounts'},
       'goal_created'                  => { message: 'あなたが目標貯金「%s」を作成しました', url: 'osidori://'},
-      'goal_created_partner'          => { message: 'パートナーが目標貯金「%s」を作成しました', url: 'osidori://financial-accounts'},
+      'goal_created_partner'          => { message: 'パートナーが目標貯金「%s」を作成しました', url: 'osidori://'},
       'goal_updated'                  => { message: '目標貯金「%s」が更新されました', url: 'osidori://'},
       'goal_monthly_accumulation'     => { message: '目標貯金「%s」に月々の積立分が貯金されました', url: 'osidori://'},
       'goal_loss'                     => { message: '目標貯金「%s」への貯金ができませんでした（残高不足）', url: 'osidori://'},
@@ -15,7 +15,7 @@ module ACTIVITY_TYPE
       'person_expend_to_fam_partner'  => { message: 'パートナーが取引を家族ホームに振り分けました', url: 'osidori://transactions/%d'},
       'person_account_to_fam'         => { message: 'あなたの口座を家族ホームに移行しました', url: 'osidori://financial-accounts'},
       'person_account_to_fam_partner' => { message: 'パートナーの口座が家族ホームに移行しました。', url: 'osidori://financial-accounts'},
-      'individual_manual_outcome'     => { message: '手動で明細が作成されました。', url: 'osidori://transactions/%d'},
+      'individual_manual_outcome'     => { message: 'あなたが取引明細を手動で作成しました', url: 'osidori://transactions/%d'},
       'fam_expend_rev'                => { message: '家族の取引が%d件ありました', url: 'osidori://transactions'},
       'individual_manual_outcome_fam' => { message: '夫婦の明細が手動で作成されました。', url: 'osidori://transactions/%d'},
   }.freeze
