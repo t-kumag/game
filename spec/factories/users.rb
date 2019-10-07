5699222739ee98923e0fc194270e47ac0c481e40
nealle-fujiura <fujiura@nealle.com>
2019-10-07 16:12:15 +0900

    add trait with_patrner_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -63,6 +63,32 @@ FactoryBot.define do
         user.at_user = create(:at_user, :with_at_user_emoney_transactions, user_id: user.id)
       end
     end
-    
+
+    trait :with_partner_user do
+      after(:create) do |user|
+        partner_user = create(:user)
+        group = create(:group)
+        create(
+          :pairing_request, 
+          from_user_id: user.id, 
+          to_user_id: partner_user.id,
+          group_id: group.id,
+          status: 2
+        )
+        create(
+          :participate_group, 
+          group_id: group.id,
+          user_id: user.id
+        )
+        create(
+          :participate_group, 
+          group_id: group.id,
+          user_id: partner_user.id
+        )
+      end
+    end
+
+    factory :pairing_user, traits: [:with_partner_user, :with_at_user_bank_accounts]
+
   end
 end
