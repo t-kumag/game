5849baea96ae1d98fe4ad12bf97513b3dff0caac
nealle-fujiura <fujiura@nealle.com>
2019-10-21 17:21:21 +0900

    modify 000_test_db_create.sh

--- docker/db/sql/000_test_db_create.sh
+++ docker/db/sql/000_test_db_create.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "CREATE DATABASE IF NOT EXISTS "$MYSQL_TEST_DATABASE" ;" | "${mysql[@]}"
-# echo "CREATE USER IF NOT EXISTS '"$MYSQL_TEST_USER"'@'%' ;" | "${mysql[@]}"
-echo "CREATE USER IF NOT EXISTS '"$MYSQL_TEST_USER"'@'127.0.0.1' ;" | "${mysql[@]}"
-# echo "SET PASSWORD FOR '"$MYSQL_TEST_USER"'@'%'=PASSWORD('"$MYSQL_PASSWORD"') ;" | "${mysql[@]}"
-echo "SET PASSWORD FOR '"$MYSQL_TEST_USER"'@'127.0.0.1'=PASSWORD('"$MYSQL_PASSWORD"') ;" | "${mysql[@]}"
-# echo "GRANT ALL ON "$MYSQL_TEST_DATABASE".* TO '"$MYSQL_TEST_USER"'@'%' ;" | "${mysql[@]}"
-echo "GRANT ALL ON "$MYSQL_TEST_DATABASE".* TO '"$MYSQL_TEST_USER"'@'127.0.0.1' ;" | "${mysql[@]}"
+echo "CREATE USER IF NOT EXISTS '"$MYSQL_TEST_USER"'@'%' ;" | "${mysql[@]}"
+echo "SET PASSWORD FOR '"$MYSQL_TEST_USER"'@'%'=PASSWORD('"$MYSQL_PASSWORD"') ;" | "${mysql[@]}"
+echo "GRANT ALL ON "$MYSQL_TEST_DATABASE".* TO '"$MYSQL_TEST_USER"'@'%' ;" | "${mysql[@]}"
 echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
