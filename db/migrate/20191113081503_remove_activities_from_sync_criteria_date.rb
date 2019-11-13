ca84d0a332087caf2d2f056d9474930dc205a166
osidori-amemiya <kenji.amemiya@osidori.co>
2019-11-13 11:23:12 +0200

    db変更

--- db/migrate/20191113081503_remove_activities_from_sync_criteria_date.rb
+++
@@ -1,9 +0,0 @@
-class RemoveActivitiesFromSyncCriteriaDate < ActiveRecord::Migration[5.2]
-  def up
-    remove_column :activities, :sync_criteria_date, :datetime
-  end
-
-  def down
-    add_column :activities, :sync_criteria_date, :datetime, after: :date
-  end
-end
