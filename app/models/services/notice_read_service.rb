de07de775091c5c6c84c58e6bc4fc74a01ef40da
osidori-amemiya <kenji.amemiya@osidori.co>
2020-01-15 14:05:23 +0200

    refactor

--- app/models/services/notice_read_service.rb
+++ app/models/services/notice_read_service.rb
@@ -1,7 +1,7 @@
 class Services::NoticeReadService
 
-  def self.already_exists?(notice, notices_mark, current_user)
-    notices_mark.find_by(notice_id: notice.id, user_id: current_user.id).present?
+  def self.already_exists?(notice, notices_read, current_user)
+    notices_read.find_by(notice_id: notice.id, user_id: current_user.id).present?
   end
 
   def self.fetch_notice_read(notice, current_user)
