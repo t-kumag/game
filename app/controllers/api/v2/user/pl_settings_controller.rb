2e5ec073d42892e3e4b4d1bde8fd1d4703bb2db2
tadashi.kumagai <human12351@gmail.com>
2020-02-27 16:41:06 +0900

    指摘修正

--- app/controllers/api/v2/user/pl_settings_controller.rb
+++ app/controllers/api/v2/user/pl_settings_controller.rb
@@ -22,8 +22,7 @@ class Api::V2::User::PlSettingsController < ApplicationController
     if @current_user.partner_user.present?
       partner_pl_setting = Entities::UserPlSetting.find_by(user_id: @current_user.partner_user.id)
       unless partner_pl_setting.present?
-        partner_pl_setting = Entities::UserPlSetting.create!(user_id: @current_user.partner_user.id)
-        partner_pl_setting.update!(pl_period_date: 1, pl_type: "")
+        partner_pl_setting = Entities::UserPlSetting.create!(user_id: @current_user.partner_user.id, pl_period_date: 1, pl_type: "")
       end
       partner_pl_setting.update!(partner_update_params)
     end
