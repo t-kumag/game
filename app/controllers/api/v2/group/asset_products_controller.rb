ba5fccd9ac2fa7af57fbdfe6356a04e84b4d92e5
osidori-ono <f.ppuci@gmail.com>
2020-03-12 18:39:01 +0900

    証券の商品一覧の並び替え対応

--- app/controllers/api/v2/group/asset_products_controller.rb
+++ app/controllers/api/v2/group/asset_products_controller.rb
@@ -6,7 +6,7 @@ class Api::V2::Group::AssetProductsController < ApplicationController
     if disallowed_at_stock_ids?([account_id])
       render_disallowed_financier_ids && return
     end
-    @asset_products = Entities::AtUserAssetProduct.where(at_user_stock_account_id: account_id)
+    @asset_products = Services::AtAssetProductService.new(@current_user).list(account_id)
     render json: {}, status: 204 and return if @asset_products.blank?
     render 'list', formats: 'json', handlers: 'jbuilder'
   end
