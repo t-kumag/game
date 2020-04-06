e7e0819c9f93d5c071f8ff2d97b979631f57ecb4
osidori-ono <f.ppuci@gmail.com>
2020-04-06 16:10:52 +0900

    例外をrender_500で返す

--- app/mailers/mail_delivery.rb
+++ app/mailers/mail_delivery.rb
@@ -3,32 +3,52 @@ class MailDelivery < ApplicationMailer
   def user_registration(user)
     @token = user.token
     @token_expires_at = Time.parse(user.token_expires_at.to_s).strftime("%Y-%m-%d %H:%M")
-    mail( :to => user.email,
-          :subject => '【OsidOri】登録を完了してはじめよう' )
+    begin
+      mail( :to => user.email,
+            :subject => '【OsidOri】登録を完了してはじめよう')
+    rescue => e
+      return render_500 e
+    end
   end
 
   def user_change_password_request(user)
     @token = user.token
-    mail( :to => user.email,
-          :subject => '【OsidOri】パスワード再設定のご連絡' )
+    begin
+      mail( :to => user.email,
+            :subject => '【OsidOri】パスワード再設定のご連絡')
+    rescue => e
+      return render_500 e
+    end
   end
   
   def account_linkage_error(user, account)
     @account = account
-    mail( :to => user.email,
-          :subject => '【OsidOri】口座連携エラーが発生しました' )
+    begin
+      mail( :to => user.email,
+            :subject => '【OsidOri】口座連携エラーが発生しました')
+    rescue => e
+      return render_500 e
+    end
   end
 
   def skip_scraping(user, account)
     @account = account
-    mail( :to => user.email,
-          :subject => '【OsidOri】口座連携エラーのためスクレイピングスキップしました。' )
+    begin
+      mail( :to => user.email,
+            :subject => '【OsidOri】口座連携エラーのためスクレイピングスキップしました。' )
+    rescue => e
+      return render_500 e
+    end
   end
 
   def user_pairing(user)
     @token = user.token
-    mail( :to => user.email,
-          :subject => '【OsidOri】ペアリング完了です！' )
+    begin
+      mail( :to => user.email,
+            :subject => '【OsidOri】ペアリング完了です！' )
+    rescue => e
+      return render_500 e
+    end
   end
 
 end
